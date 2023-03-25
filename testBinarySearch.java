761636546993497a3058e555d63bca648d90d8c9
Anamitra Datta <anamitradatta@gmail.com>
2023-03-25 14:50:26 -0400

    update binary search functions

--- testBinarySearch.java
+++ testBinarySearch.java
@@ -1,15 +1,13 @@
 package test;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 public class testBinarySearch {
 
-	// Recursive
-	public static int binarySearchRec(int[] arr, int start, int end, int key)
+	// Recursive - binary Search Zero - find index, if key not in array, return -1
+	// Zero - finds index if possible (zero indices end con)
+	public static int binarySearchZero_Rec(int[] arr, int start, int end, int target)
 	{
+		if(arr==null || arr.length==0) return -1;
+		
 		if(start>end)
 		{
 			return -1;
@@ -17,13 +15,13 @@ public class testBinarySearch {
 		
 		int middle=start+(end-start)/2;
 		
-		if(arr[middle]<key)
+		if(arr[middle]<target)
 		{
-			return binarySearchRec(arr,middle+1,end,key);
+			return binarySearchZero_Rec(arr,middle+1,end,target);
 		}
-		else if(key<arr[middle])
+		else if(target<arr[middle])
 		{
-			return binarySearchRec(arr,start,middle-1,key);
+			return binarySearchZero_Rec(arr,start,middle-1,target);
 		}
 		else
 		{
@@ -31,18 +29,21 @@ public class testBinarySearch {
 		}
 	}
 	
-	// Iterative
-	public static int binarySearchIt(int[] arr, int start, int end, int key)
+	// Iterative - binary Search Zero - find index, if key not in array, return -1
+	// Zero - finds index if possible (zero indices end con)
+	public static int binarySearchZero_It(int[] arr, int start, int end, int target)
 	{
+		if(arr==null || arr.length==0) return -1;
+		
 		while(start<=end)
 		{
 			int middle = start + (end-start)/2;
 			
-			if(arr[middle] < key)
+			if(arr[middle] < target)
 			{
 				start = middle + 1;
 			}
-			else if(arr[middle]>key)
+			else if(arr[middle]>target)
 			{
 				end = middle - 1;
 			}
@@ -54,33 +55,85 @@ public class testBinarySearch {
 		return -1;
 	}
 	
-	// Find Proper Index to place element before/at
-	public static int binarySearchIndex(int[] arr, int start, int end, int target) 
+	// Iterative - binary Search One - find index with one remaining index
+	// One - gets one index, if condition not met, returns -1 (one index end con)
+	public static int binarySearchOne_It(int[] arr, int start, int end, int target) 
 	{
+		if(arr==null || arr.length==0) return -1;
+		
 	    while (start < end) 
 	    {
 	    	int mid = start + (end - start)/2;
-	    	if (arr[mid] >= target) 
+	    	if (arr[mid] > target) 
 	    	{
 	    		end = mid;
 	    	}
-	    	else 
+	    	else if (arr[mid] < target)
 	    	{
 	    		start = mid + 1;
 	    	}
+	    	else
+	    	{
+	    		return mid;
+	    	}
 	    }
-	    return end;
+	    
+	    // end==start
+	    if(start < arr.length && start>=0 && arr[start]==target)
+	    {
+	    	return start;
+	    }
+	    else
+	    {
+	    	return -1;
+	    }
+	}
+	
+	// Iterative - binary Search Two - find index with one remaining index
+	// Two - gets two indices, if condition not met, returns -1 (two index end con)
+	public static int binarySearchTwo_It(int[] arr, int start, int end, int target) 
+	{
+		if(arr==null || arr.length==0) return -1;
+			
+		while(start+1<end)
+		{
+			int mid = start + (end - start)/2;
+			if(arr[mid] < target)
+			{
+				start = mid;
+			}
+			else if(arr[mid] > target)
+			{
+				end = mid;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+		
+		if(start < arr.length && start>=0 && arr[start]==target)
+	    {
+	    	return start;
+	    }
+		if(end < arr.length && end>=0 && arr[end]==target)
+	    {
+	    	return end;
+	    }
+		return -1;
+		
 	}
 
 	public static void main(String[] args) 
 	{
-		int[] arr = new int[] {1,2,3,4,5,6,7,8,100,1000};
+		int[] arr = new int[] {1,2,3,4,5,6,7,8,100,919,1000};
 		
-		System.out.println("Recursive = " + binarySearchRec(arr,0,arr.length-1,1000));
-		System.out.println("Iterative = " + binarySearchIt(arr,0,arr.length-1,1000));
-		System.out.println("Index = " + binarySearchIndex(arr,0,arr.length-1,99));
+		int num=919;
 		
-		
+		System.out.println("Recursive binary Search (zero indices end con) = " + binarySearchZero_Rec(arr,0,arr.length-1,num));
+		System.out.println("Iterative binary Search (zero indices end con) = " + binarySearchZero_It(arr,0,arr.length-1,num));
+		System.out.println("Iterative binary Search (one index end con) = " + binarySearchOne_It(arr,0,arr.length-1,num));
+		System.out.println("Iterative binary Search (two index end con) = " + binarySearchTwo_It(arr,0,arr.length-1,num));
 	}
 
 }
