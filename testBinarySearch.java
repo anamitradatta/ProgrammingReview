f12a1bbf03fe8a14c096bea2175cdc5ada6ccf85
Anamitra Datta <anamitradatta@gmail.com>
2023-06-18 23:02:45 -0400

    add alt bs two index

--- testBinarySearch.java
+++ testBinarySearch.java
@@ -89,7 +89,7 @@ public class testBinarySearch {
 	    }
 	}
 	
-	// Iterative - binary Search Two - find index with one remaining index
+	// Iterative - binary Search Two - find index with two remaining indices
 	// Two - gets two indices, if condition not met, returns -1 (two index end con)
 	public static int binarySearchTwo_It(int[] arr, int start, int end, int target) 
 	{
@@ -123,11 +123,45 @@ public class testBinarySearch {
 		return -1;
 		
 	}
+	
+	// Iterative - binary Search Two - find index with two remaining indices
+	// Two - gets two indices, if condition not met, returns -1 (two index end con)
+	// can also add 1 to midpoint calc for window sizes of two for normal check
+	// if +1 is not added, it will result in infinite loop
+	public static int binarySearchTwoAlt_It(int[] arr, int start, int end, int target) 
+	{
+		if(arr==null || arr.length==0) return -1;
+				
+		while(start<end)
+		{
+			int mid = start + (end - start + 1)/2;
+			if(arr[mid] < target)
+			{
+				start = mid;
+			}
+			else if(arr[mid] > target)
+			{
+				end = mid;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+			
+		if(end < arr.length && end>=0 && arr[end]==target)
+	    {
+	    	return end;
+	    }
+		return -1;
+		
+	}
+	
 	public static void main(String[] args) 
 	{
-		int[] arr = new int[] {-203,-102,-10,1,2,5,18,22,27,100,101,114};
+		int[] arr = new int[] {-203,-102,-10,1,2,5,18,22,27,100,101,114,700};
 		
-		int num=5;
+		int num=101;
 		
 		if(!UtilityAlgs.arrayIsSorted(arr)) 
 		{ 
@@ -139,6 +173,7 @@ public class testBinarySearch {
 		System.out.println("Iterative binary Search (zero indices end con) = " + binarySearchZero_It(arr,0,arr.length-1,num));
 		System.out.println("Iterative binary Search (one index end con) = " + binarySearchOne_It(arr,0,arr.length-1,num));
 		System.out.println("Iterative binary Search (two indices end con) = " + binarySearchTwo_It(arr,0,arr.length-1,num));
+		System.out.println("Iterative binary Search (two indices alt end con) = " + binarySearchTwoAlt_It(arr,0,arr.length-1,num));
 	}
 
 }
